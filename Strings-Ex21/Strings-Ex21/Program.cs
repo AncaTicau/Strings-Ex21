c55d3f92320b78e379acecae49f50e35c9a00e9f
MariusA <anca.ticau@gmail.com>
2019-06-12 20:22:47 +0300

    Strings-Ex21

---
+++ Strings-Ex21/Strings-Ex21/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Strings_Ex21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string givenString = "This is a string";
+            int givenIndex = 3;
+            Console.WriteLine(RemoveChar(givenString, givenIndex));
+        }
+        //Write a method that to remove the nth index character from a nonempty string.
+        public static string RemoveChar(string input, int index)
+        {
+            if (index > input.Length)
+            {
+                return input;
+            }
+
+            string result = input.Remove(index - 1, 1);
+
+            return result;
+        }
+    }
+}
